9e1bd4f9d9958c478b0277a208361740c8ae5d20
csosap <149740862+csosap@users.noreply.github.com>
2023-11-02 19:48:07 -0500

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,110 @@
+﻿[
+ {
+     "Agno": 2022,
+     "Periodo": "Octubre",
+     "Parametro": "Octubre",
+     "Valor": 1,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2022,
+     "Periodo": "Noviembre",
+     "Parametro": "Noviembre",
+     "Valor": 3,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2022,
+     "Periodo": "Diciembre",
+     "Parametro": "Diciembre",
+     "Valor": 11,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Enero",
+     "Parametro": "2023",
+     "Valor": 7,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Febrero",
+     "Parametro": "Febrero",
+     "Valor": 2,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Marzo",
+     "Parametro": "Marzo",
+     "Valor": 2,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Abril",
+     "Parametro": "Abril",
+     "Valor": 4,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Mayo",
+     "Parametro": "Mayo",
+     "Valor": 6,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Junio",
+     "Parametro": "Junio",
+     "Valor": 4,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Julio",
+     "Parametro": "Julio",
+     "Valor": 8,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Agosto",
+     "Parametro": "Agosto",
+     "Valor": 7,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Septiembre",
+     "Parametro": "Septiembre",
+     "Valor": 10,
+     "Estado": "Definitivo",
+     "Notas": ""
+ },
+ {
+     "Agno": 2023,
+     "Periodo": "Octubre",
+     "Parametro": "Octubre",
+     "Valor": 1,
+     "Estado": "Definitivo",
+     "Notas": ""
+ }
+]
+           
+
+        
+    
